3f3289dd11a26d6a00205c553273938709950cdc
Jeremy Kersten <jke@odoo.com>
2020-09-03 17:55:35 +0200

    [3] add model - no coupon

--- oxp_loyalty/models/models.py
+++ oxp_loyalty/models/models.py
@@ -1,18 +1,44 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import fields, api, models, _
 
 
-# class oxp_loyalty(models.Model):
-#     _name = 'oxp_loyalty.oxp_loyalty'
-#     _description = 'oxp_loyalty.oxp_loyalty'
+class LoyaltyHistory(models.Model):
+    _name = "loyalty.history"
+    _description = "Loyalty History"
+    _order = 'id desc'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    name = fields.Char()
+    partner_id = fields.Many2one('res.partner', required=1)
+    amount = fields.Integer()
+    order_id = fields.Many2one('sale.order')
+    coupon_id = fields.Many2one('coupon.program')
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    allow_loyalty = fields.Boolean(default=True, help="Offer Oxp when customer buy it")
+
+    def _compute_label(self):
+        self.ensure_one()
+        return self.allow_loyalty and _('Win %s Oxp by buying this product', self.price * 100) or ''
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    loyalty_ids = fields.One2many('loyalty.history', 'partner_id')
+    loyalty_amount = fields.Integer(compute='_compute_loyalties', store=True)
+
+    @api.depends('loyalty_ids', 'loyalty_ids.amount')
+    def _compute_loyalties(self):
+        for p in self:
+            # could by done in groupby if displayed in multi
+            p.loyalty_amount = sum(p.loyalty_ids.mapped('amount'))
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    loyalty_ids = fields.One2many('loyalty.history', 'order_id')
