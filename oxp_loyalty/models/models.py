fb7afecefc49ec8f9d30f452105645a87fe150f6
Jeremy Kersten <jke@odoo.com>
2020-09-03 18:59:01 +0200

    [IMP] compute loyalties on confirm

--- oxp_loyalty/models/models.py
+++ oxp_loyalty/models/models.py
@@ -3,6 +3,41 @@
 from odoo import fields, api, models, _
 
 
+class LoyaltyPack(models.Model):
+    _name = "loyalty.pack"
+    _description = "Loyalty Pack"
+
+    name = fields.Char(required=1)
+    oxp = fields.Integer(default=100, required=1)
+    program_id = fields.Many2one('coupon.program', required=1)
+
+    def _convert(self, send_mail=True):
+        partner = self.env.user.partner_id
+        assert partner.loyalty_amount >= self.point
+
+        coupon = self.env['sale.coupon'].create({
+            'program_id': self.program_id.id,
+            'partner_id': partner.id,
+        })
+
+        self.env['loyalty.history'].create({
+            "partner_id": partner.id,
+            "amount": -1 * self.point,
+            "order_id": False,
+            "name": _('Pack %s: %s' % (self.name, coupon.code)),
+            "status": 'confirm',
+            "coupon_id": coupon.id,
+        })
+        if send_mail:
+            template = self.env.ref('sale_coupon.mail_template_sale_coupon', raise_if_not_found=False)
+            self.env['mail.thread'].message_post_with_template(
+                template.id, composition_mode='comment',
+                model='sale.coupon', res_id=coupon.id,
+                email_layout_xmlid='mail.mail_notification_light',
+            )
+        return coupon
+
+
 class LoyaltyHistory(models.Model):
     _name = "loyalty.history"
     _description = "Loyalty History"
@@ -42,3 +77,29 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     loyalty_ids = fields.One2many('loyalty.history', 'order_id')
+
+    def action_cancel(self):
+        res = super().action_cancel()
+
+        # get back all loyalties for this order
+        for loy in self.loyalty_ids.filtered(lambda l: l.amount > 0):
+            self.env['loyalty.history'].create({
+                "partner_id": self.partner_id.id,
+                "amount": -1 * loy.amount,
+                "order_id": self.id,
+                "name": 'Cancel order %s' % self.id,
+            })
+        return res
+
+    def action_confirm(self):
+        res = super().action_confirm()
+        self.env['loyalty.history'].sudo().create({
+            "partner_id": self.partner_id.id,
+            "amount": self._get_amount_loyalty(),
+            "order_id": self.id,
+            "name": "Order %s" % self.name,
+        })
+        return res
+
+    def _get_amount_loyalty(self):
+        return sum(self.order_line.filtered(lambda r: r.product_id.product_tmpl_id.allow_loyalty).mapped('price_total') * 100)
