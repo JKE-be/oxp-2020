9905ba3c13e7c639deb4d62d067d96c207759804
Jeremy Kersten <jke@odoo.com>
2020-09-03 17:45:06 +0200

    [2] adapt manifest to oxp project

--- oxp_loyalty/__manifest__.py
+++ oxp_loyalty/__manifest__.py
@@ -1,35 +1,23 @@
 # -*- coding: utf-8 -*-
 {
     'name': "oxp_loyalty",
+    'author': "Odoo SA - Jeremy Kersten",
+    'website': "https://www.odoo.com",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'category': 'Website/Website',
+    'version': '0.1',
+    'license': 'LGPL-3',
 
     'description': """
-        Long description of module's purpose
+        Win 'Oxp' during your online shopping and convert it into coupon !
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'depends': ['base', 'website_sale_coupon'],  # website_sale + coupon
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+        'data/data.xml',
     ],
 }
